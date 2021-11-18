466476cae2a7a405ae75eb927e6256142ba34429
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 16:41:19 +0100

    fix: Fix method name.

--- what-else.js
+++ what-else.js
@@ -1,1 +1,1 @@
-console.log(vips.include('George Clooney') ? 'What else ?' : 'The only failure is not to try.');
+console.log(vips.includes('George Clooney') ? 'What else ?' : 'The only failure is not to try.');
