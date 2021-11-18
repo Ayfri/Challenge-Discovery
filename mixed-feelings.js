c437f5d20d56da1f8a6cbc1c61ee3b70009acf69
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 11:24:01 +0100

    feat: Add quest 2 challenge 8.

---
+++ mixed-feelings.js
@@ -0,0 +1,22 @@
+const emotions = {
+	happiness: {
+		level: 0,
+	},
+	sadness: {
+		level: 0,
+	},
+	disgust: {
+		level: 0,
+	},
+	fear: {
+		level: 0,
+	},
+	anger: {
+		level: 0,
+	},
+	surprise: {
+		level: 0,
+	},
+};
+
+const mixedFeelings = [emotions[0], emotions[1], emotions[2]];
