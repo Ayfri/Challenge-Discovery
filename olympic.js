47a2354f6d6a3cd7521e60f9b17f035cea514bac
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 16:56:36 +0100

    fix: Fix positions.

--- olympic.js
+++ olympic.js
@@ -1,11 +1,11 @@
 switch (athletes.indexOf('Pedro')) {
-	case 1:
+	case 0:
 		console.log('🥇');
 		break;
-	case 2:
+	case 1:
 		console.log('🥈');
 		break;
-	case 3:
+	case 2:
 		console.log('🥉');
 		break;
 	default:
