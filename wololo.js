b05ebadfb61d8ddd20eac84e5f3af7706b99c299
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 15:01:54 +0100

    feat: Add quest 3 challange 13.

---
+++ wololo.js
@@ -0,0 +1,6 @@
+const stringFromNumber =  String(num);
+const stringFromBoolean = String(bool);
+const numberFromString = Number(string);
+const numberFromBoolean = Number(boolean);
+const booleanFromString = Boolean(string);
+const booleanFromNumber = Boolean(number);
