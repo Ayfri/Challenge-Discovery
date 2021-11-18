eb97701cba8442a8eb477d35c92330c5a99e5995
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 15:17:45 +0100

    feat: Add quest 4 challenge 1.

---
+++ griswold-the-blacksmith.js
@@ -0,0 +1,4 @@
+const purchasableGoods = [];
+if (playerCoins > sword) purchasableGoods.push(sword);
+if (playerCoins > boots) purchasableGoods.push(boots);
+if (playerCoins > arrows) purchasableGoods.push(arrows);
