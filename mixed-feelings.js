9568ee7f8c48955ea333429267635c730ff946a6
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 11:26:58 +0100

    fix: Fix objecst pattern.

--- mixed-feelings.js
+++ mixed-feelings.js
@@ -1,22 +1,28 @@
-const emotions = {
-	happiness: {
-		level: 0,
+const emotions = [
+	{
+		type: 'happiness',
+		level: 0
 	},
-	sadness: {
-		level: 0,
-	},
-	disgust: {
-		level: 0,
-	},
-	fear: {
-		level: 0,
-	},
-	anger: {
-		level: 0,
-	},
-	surprise: {
-		level: 0,
-	},
-};
+	{
+        type: 'sadness',
+        level: 0
+    },
+    {
+        type: 'anger',
+        level: 0
+    },
+    {
+        type: 'fear',
+        level: 0
+    },
+    {
+        type: 'disgust',
+        level: 0
+    },
+    {
+        type: 'surprise',
+        level: 0
+    }
+]
 
 const mixedFeelings = [emotions[0], emotions[1], emotions[2]];
