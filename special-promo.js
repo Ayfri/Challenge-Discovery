f093a805cbafabdaf399654e72bc8cd1441264a9
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 15:44:15 +0100

    fix: Fix conditions.

--- special-promo.js
+++ special-promo.js
@@ -1,1 +1,1 @@
-if (user.age < 18 && user.age >= 25 && user.activeMembership) console.log('You can benefit from our special promotion');
+if (user.age > 18 && user.age <= 25 && user.activeMembership) console.log('You can benefit from our special promotion');
