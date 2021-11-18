0113e733d5ed450d881706c851638956139e78d3
Ayfri <pierre.ayfri@gmail.com>
2021-11-18 15:09:31 +0100

    feat: Add quest 3 challange 15.

---
+++ rest-in-peace.js
@@ -0,0 +1,3 @@
+const footSoldiers = ['Grim the Terrible', ...swordMen, ...archers];
+const horseMen = ['The Skull Knight', ...captains, ...generals];
+const armyOfTheDead = ['The King of the Dead', ...footSoldiers, ...horseMen];
